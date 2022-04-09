6c2cfa5c38644b9d3e7495ae47f989726a70cec9
Dusan Sluka <68845210+Dusko-8@users.noreply.github.com>
2022-04-09 22:01:41 +0200

    Set up TESTS

---
+++ src/Math_Library.UnitTests/MathLibraryTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Math_Library.UnitTests
+{
+    [TestClass]
+    public class MathLibraryTests
+    {
+        [TestMethod]
+        public void Plus_scenarios()
+        {
+        }
+    }
+}
